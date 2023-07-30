f9d5d252bf2da98137d338e12b6ddeeed679c551
Rahul <rahul.mm@inapp.com>
2023-07-30 22:19:30 +0530

    Completed profile form UI

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
 import Topbar from "./scenes/global/Topbar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Dashboard from './scenes/dashboard/Dashboard';
-import { Route,Routes } from "react-router-dom";
-import Sidebar from './scenes/global/Sidebar'
-import Team from './scenes/team'
-import Invoices from './scenes/invoices'
-import Contacts from './scenes/contacts'
+import Dashboard from "./scenes/dashboard/Dashboard";
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./scenes/global/Sidebar";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
 // import Bar from './scenes/bar'
-// import Form from './scenes/form'
+import Form from "./scenes/form";
 // import Line from './scenes/line'
 // import Pie from './scenes/pie'
 // import FAQ from './scenes/faq'
 // import Geography from './scenes/Geography'
 
 function App() {
-
-   const [theme, colorMode] = useMode();
+  const [theme, colorMode] = useMode();
   return (
-     <ColorModeContext.Provider value={colorMode}>
+    <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline/>
-    <div className="app">
-      <Sidebar/>
-      <main className="content">
-        <Topbar/> 
-        <Routes>
-          <Route path="/" element={<Dashboard/>}/>
-           <Route path="/team" element={<Team/>}/> 
-          <Route path="/contacts" element={<Contacts/>}/>
-          <Route path="/invoices" element={<Invoices/>}/>
-          {/* <Route path="/bar" element={<Bar/>}/> */}
-          {/* <Route path="/form" element={<Form/>}/> */}
-          {/* <Route path="/line" element={<Line/>}/> */}
-          {/* <Route path="/pie" element={<Pie/>}/> */}
-          {/* <Route path="/faq" element={<FAQ/>}/> */}
-          {/* <Route path="/geography" element={<Geography/>}/>  */}
-        </Routes>
-        </main>
-    </div>
-      </ThemeProvider> 
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/bar" element={<Bar/>}/> */}
+              <Route path="/form" element={<Form />} />
+              {/* <Route path="/line" element={<Line/>}/> */}
+              {/* <Route path="/pie" element={<Pie/>}/> */}
+              {/* <Route path="/faq" element={<FAQ/>}/> */}
+              {/* <Route path="/geography" element={<Geography/>}/>  */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
